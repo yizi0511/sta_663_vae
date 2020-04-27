39673938197196d644a4718e0fc57a25303822a4
yizi <yizzhang@ucdavis.edu>
2020-04-27 15:56:11 -0400

    updated demo

--- vae_hcp_pytorch.py
+++ vae_hcp_pytorch.py
@@ -1,19 +1,11 @@
-from __future__ import print_function
 import argparse
-import torch
-import torch.nn.functional as F
-from torch import nn, optim
-from torchvision import datasets, transforms
-from tqdm import tqdm
-from scipy.io import loadmat
-from torch.autograd import Variable
 import numpy as np
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from torch.autograd import Variable
 import torch.utils.data as utils
-from torchvision.utils import save_image
-import matplotlib.pyplot as plt
-
-
-device = torch.device("cuda")
+from scipy.io import loadmat
 
 
 def to_var(x, requires_grad=False, volatile=False):
@@ -46,13 +38,16 @@ class VAE(nn.Module):
         self.fc45 = nn.Linear(68,68)
         self.fcy1 = nn.Linear(latent_dim,1)
         self.fcintercept = nn.Linear(68*68, 68*68)
+
     def encode(self, x):
         h1 = F.relu(self.fc1(x))
         return self.fc21(h1), self.fc22(h1)
+
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5*logvar)
         eps = torch.randn_like(std)
         return mu + eps*std
+
     def decode(self, z):
         h31= F.sigmoid(self.fc31(z))
         h31= F.sigmoid(self.fc41(h31))
@@ -74,6 +69,7 @@ class VAE(nn.Module):
         h30 = self.a.expand(batch_size, 68*68) + h30.view(-1,68*68)
         h30 = self.fcintercept(h30)
         return h30.view(-1, 68*68)
+
     def forward(self, x):
         mu, logvar = self.encode(x.view(-1, 68*68))
         z = self.reparameterize(mu, logvar)
@@ -82,14 +78,11 @@ class VAE(nn.Module):
         return recon.view(-1, 68*68), mu, logvar, trait.view(-1,1)
 
 
-
 def loss_function(recon_x, that, x, t , mu, logvar):
     BCE = F.poisson_nll_loss(recon_x , x, reduction='sum', log_input=True)
     NCE = F.mse_loss(that, t, reduction='sum')
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     return 0.01*(BCE + KLD) + NCE
-
-
 
 
 def train(epoch, train_loader_k):
@@ -130,9 +123,8 @@ def test(epoch, test_loader_k, test_index):
     return test_loss, pred_val, test_val
 
 
-
-
-dat_mat = loadmat('../HCP_12_2019/HCP_subcortical_CMData_desikan.mat')
+### Load brain graphs data
+dat_mat = loadmat('./vae/data/HCP_subcortical_CMData_desikan.mat')
 tensor = dat_mat['loaded_tensor_sub']
 net_data = []
 for i in range(tensor.shape[3]):
@@ -142,37 +134,38 @@ for i in range(tensor.shape[3]):
     ith = ith.reshape(68*68)
     net_data.append(ith)
 
-
-net_mean = np.mean(net_data, axis=0)
+### Load human cognition traits
+# 4 - Oral Reading Recognition Test
+# 6 - Picture Vocabulary Test
+# 24 - Line Orientation: Total number correct
+# 26 - Line Orientation: Total positions off for all trials
 tensor_y = torch.stack([torch.Tensor(i) for i in net_data])
-phen_dat = loadmat('../HCP_12_2019/HCP_Covariates.mat')
+phen_dat = loadmat('./vae/data/HCP_Covariates.mat')
 phen_idx = [np.where(phen_dat['allsubject_id'] == ival)[0] for ival in dat_mat['all_id'].squeeze()]
-trait_data = phen_dat['cog_measure'][24, phen_idx]
+trait_data = phen_dat['cog_measure'][4, phen_idx]
 trait_data[np.isnan(trait_data)] = np.nanmean(trait_data)
 trait_data = (trait_data-np.mean(trait_data))/np.std(trait_data)
 trait_data = torch.from_numpy(trait_data)
 trait_data = trait_data.float()
-# 4 - Oral Reading Recognition Test
-# 6 - Picture Vocabulary Test 
-# 24 - Line Orientation: Total number correct
-# 26 - Line Orientation: Total positions off for all trials 
 
-
+### Create data sets
 batch_size = 5
-y = utils.TensorDataset(tensor_y, trait_data) # create your datset
+y = utils.TensorDataset(tensor_y, trait_data)
 train_loader = utils.DataLoader(y, batch_size) 
 
+
+### Training
 nepoch = 50
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 model = VAE().to(device)
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-for j in range(100):
+for j in range(nepoch):
     train(j, train_loader)
     valid_loss, traithat, trait = test(j, train_loader, np.arange(len(train_loader.dataset)))
 
 
-
+### Obtain latent z for visualization
 torch.cuda.empty_cache()
 
 latent_dim=68
@@ -180,9 +173,6 @@ num_elements = len(train_loader.dataset)
 num_batches = len(train_loader)
 batch_size = train_loader.batch_size
 mu_out = torch.zeros(num_elements, latent_dim)
-logvar_out = torch.zeros(num_elements,latent_dim)
-recon_out = torch.zeros(num_elements,68*68)
-x_latent_out = torch.zeros(num_elements, 68)
 with torch.no_grad():
     for i, (data, trait) in enumerate(train_loader):
         start = i*batch_size
@@ -193,6 +183,5 @@ with torch.no_grad():
         trait = trait.to(device)
         recon_batch, mu, logvar, traithat = model(data)
         mu_out[start:end] = mu
-
 
 np.save('mu.npy', mu_out.detach().numpy())
